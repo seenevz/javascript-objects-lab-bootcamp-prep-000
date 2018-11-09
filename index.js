7d3b755b76501a3c76495d15cf2eb11680e2a1f8
Sergio Neves <serneves@hotmail.com>
2018-11-09 13:24:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  return object[key] = value
 }
