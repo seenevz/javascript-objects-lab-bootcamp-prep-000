024255dbba409a4c48b6dba3d94b37e0be9d27c9
Sergio Neves <serneves@hotmail.com>
2018-11-09 12:48:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var recipes = new Object ()
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
